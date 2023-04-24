be1e5fa2ac11cf2578ce071cd409bb88ab65c799
DenisGuzykevych <118122975+DenisGuzykevych@users.noreply.github.com>
2023-04-24 13:08:26 +0300

    Add files via upload

---
+++ PZ 4.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        Console.Write("Введiть перше слово: ");
+
+        string word1 = Console.ReadLine().ToLower();
+
+        Console.Write("Введiть друге слово: ");
+
+        string word2 = Console.ReadLine().ToLower();
+
+        bool wordsequal = true;
+
+        foreach (char letter in word2)
+
+        {
+
+            int index = word1.IndexOf(letter);
+
+            if (index == -1)
+
+            {
+
+                wordsequal = false;
+
+                break;
+
+            }
+
+            word1 = word1.Remove(index, 1);
+
+        }
+
+        if (wordsequal)
+
+        {
+
+            Console.WriteLine("З першого слова можна скласти друге");
+
+        }
+
+        else { Console.WriteLine("З першого слова не можна скласти друге"); }
+
+        Console.ReadLine();
+
+    }
+
+}
